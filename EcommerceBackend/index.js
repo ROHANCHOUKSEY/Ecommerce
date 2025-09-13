9842ce9fdee088f9b3ca1030099904cead38eb64
ROHANCHOUKSEY <rohanchouksey02@gmail.com>
2025-09-13 20:51:19 +0530

    ConnectWithFrontend

--- EcommerceBackend/index.js
+++ EcommerceBackend/index.js
@@ -2,11 +2,20 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const authRouter = require("./Router/AuthenticationRouter");
 
 app.use(cookieParser());
 app.use(express.json());
+
+app.use(
+  cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+    methods:["POST", "GET", "DELETE", "PUT"]
+  })
+)
 
 app.use("/user", authRouter);
 
